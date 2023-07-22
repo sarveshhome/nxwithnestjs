d59f749f61d7d0c5d04b1504986ed575ff79a6be
sarveshhome <sarvesh.carlton@gmail.com>
2023-07-22 21:10:39 +0530

    request

--- apps/nxwithnest/src/app/app.controller.ts
+++ apps/nxwithnest/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
@@ -9,5 +9,10 @@ export class AppController {
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Get('/add-two-numbers')
+  getAdd(@Body() body) {
+    return this.appService.calculate_Add(body);
   }
 }
--- apps/nxwithnest/src/app/app.service.ts
+++ apps/nxwithnest/src/app/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getData(): { message: string } {
     return { message: 'Hello API' };
   }
+  calculate_Add(body:any): number {
+   return 2;
+  }
+
 }
