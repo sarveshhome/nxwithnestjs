395e2c321364347ce2ee8eb16e9040cb72bab10a
sarveshhome <sarvesh.carlton@gmail.com>
2023-07-22 23:06:01 +0530

    add feature

--- apps/nxwithnest/src/app/app.controller.ts
+++ apps/nxwithnest/src/app/app.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, Post, Header } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Header,
+  Res,
+  Response,
+  HttpStatus,
+  HttpCode,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { CreateNumberDto } from './Models/createNumberDto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
@@ -16,7 +26,8 @@ export class AppController {
   @Post('/add-two-numbers')
   @Header('Content-Type', 'application/json')
   @ApiOperation({ summary: 'Get Add' })
-  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'result' })
+  @HttpCode(204)
   getAdd(@Body() body: CreateNumberDto): number {
     return this.appService.calculate_Add(body.firstNumber, body.secondNumber);
   }
